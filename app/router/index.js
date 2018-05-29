75eddf2a44b367c9a6629d3f8e453579cf8554fc
Fengxing <272355332@qq.com>
2018-05-29 09:13:20 +0800

    框架搭建完成

--- app/router/index.js
+++ app/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Home from '@/views/home/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
     routes: [{
         path: '/',
-        name: 'HelloWorld',
-        component: HelloWorld,
+        name: 'Home',
+        component: Home,
     }],
 });
